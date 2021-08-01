1a92a4911b1bdc2d9bd0cd4eafba5899d1c854ef
Mabaraka <davimraba2020@gmail.com>
2021-08-01 22:29:29 +0900

    First solution 2.3.1 1) Все согласно условиям задачи

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import web.dao.UserService;
 import web.models.User;
+import web.service.UserService;
 
 
 @Controller
