444200fc8a63aa2f5a001eb2baa6b10458e5ecf1
Mabaraka <davimraba2020@gmail.com>
2021-08-03 22:10:48 +0900

    First solution 2.3.1 1) Все согласно условиям задачи

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,36 @@
+package web.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.models.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<User> index() {
+        return entityManager.createQuery("FROM User").getResultList();
+    }
+
+    public User show(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    public void update(int id, User user) {
+        entityManager.merge(user);
+    }
+
+    public void delete(int id) {
+        entityManager.remove(show(id));
+    }
+}
